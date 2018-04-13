86d5038750be5d05990ea1da8af22ee82d74d6f4
Rohan <32576786+rohansaha@users.noreply.github.com>
2018-04-13 11:26:17 -0500

    Add files via upload

--- tests.py
+++ tests.py
@@ -7,19 +7,22 @@ Created on Sun Mar 25 11:41:10 2018
 
 import math
 
+'''True - 1
+False - 0'''
 def Euler(n, base):
     # n is the variable being tested for primality
     # base should be coprime to number
     if (base**((n - 1) / 2) - 1) % n == 0 or (base**((n - 1) / 2) + 1) % n == 0:
-        return True
+        return 1
     else:
-        return False
-
+        return 0
+'''Even - 1
+Odd - 0'''
 def even(n):
     if n % 2 == 0:
-        return "Even"
+        return 1
     else:
-        return "Odd"
+        return 0
 
 def factor(n, primes):
     factors = []
@@ -32,16 +35,21 @@ def factor(n, primes):
                 n = n // p
     return factors
 
+'''True - 1
+False - 0'''
 def Fermat(n, base):
     # number is the variable being tested for primality
     # base should be coprime to number
     if n < 2:
         return None
     elif (base**(n - 1) - 1) % n == 0:
-        return True
+        return 1
     else:
-        return False
+        return 0
 
+
+'''True - 1
+False - 0'''
 # Calculates the nth Fibonacci number
 # This needs work
 # It overflows if n gets too large
@@ -53,16 +61,20 @@ def Fibonacci(n):
     fib = round(Phi**n - phi**n / math.sqrt(5))
     
     if fib % n == 0:
-        return True
+        return 1
     else:
-        return False
+        return 0
 
+'''True - 1
+False - 0'''
 def GaussianPrime(n, primes):
     if n in primes and n % 4 == 3:
-        return True
+        return 1
     else:
-        return False
+        return 0
 
+'''True - 1
+False - 0'''
 # Calculates the nth Lucas number
 # if L_n 
 def Lucas(n):
@@ -77,9 +89,9 @@ def Lucas(n):
         lucas = round(Phi**n + phi**n)
     
     if lucas % n == 0:
-        return True
+        return 1
     else:
-        return False
+        return 0
 
 # Count the number of distinct divisors
 def countDivisors(n):
@@ -97,14 +109,16 @@ def countPrimeFactors(n, primes):
             count += 1
     return count
 
+'''True - 1
+False - 0'''
 # If a given number has more divisors than any natural number below it, it is highly composite
 def highlyComposite(n):
     current = countDivisors(n)
     for i in range(1, n):
         count = countDivisors(i)
         if count >= current:
-            return False
-    return True
+            return 0
+    return 1
 
 def mod4(i):
     result = i % 4
@@ -118,6 +132,8 @@ def mod6(i):
     result = i % 6
     return result
 
+'''True - 1
+False - 0'''
 # If all the prime factors of a given number are repeated, it is a powerful number
 def powerful(n, primes):
     factors = factor(n, primes)
@@ -125,17 +141,21 @@ def powerful(n, primes):
     for i in range(len(factors)):
        count = factors.count(factors[i])
        if count == 1:
-           return False
-    return True
+           return 0
+    return 1
 
+'''True - 1
+False - 0'''
 # If a given number is the product of k * (k + 1), it is pronic
 def pronic(n):
     k = math.floor(math.sqrt(n))
     if n == k * (k + 1):
-        return True
+        return 1
     else:
-        return False
+        return 0
 
+'''True - 1
+False - 0'''
 # If none of the prime factors of a given number are repeated, it is squarefree
 def squarefree(n, primes):
     factors = factor(n, primes)
@@ -143,5 +163,5 @@ def squarefree(n, primes):
     for i in range(len(factors)):
         count = factors.count(factors[i])
         if count > 1:
-            return False
-    return True
+            return 0
+    return 1
