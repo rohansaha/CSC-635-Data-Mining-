7ad3837c86bca9733652600fef0cccf55bd09415
Nathan Hall <35374924+NathanLHall@users.noreply.github.com>
2018-04-16 11:13:24 -0500

    Update Generate Data.py

--- Generate Data.py
+++ Generate Data.py
@@ -14,14 +14,13 @@ def main(index):
         writer = csv.writer(csvfile, delimiter=',')
         
         writer.writerow(["Number","# of Divisors","# of Factors","Fermat",
-                         "Fibonacci","Gaussian Prime","Highly Composite","Mod 5",
+                         "Gaussian Prime","Highly Composite","Mod 5",
                          "Mod 6","Powerful","Prime","Pronic","Squarefree"])
 
         for i in range(2, index):
             countDivisors = tests.countDivisors(i)
             countFactors = tests.countPrimeFactors(i, primes)
             Fermat = tests.Fermat(i,2)
-            Fibonacci = tests.Fibonacci(i)
             Gauss = tests.GaussianPrime(i, primes)
             highlyComposite = tests.highlyComposite(i)
             mod5 = tests.mod5(i)
@@ -32,10 +31,10 @@ def main(index):
             squarefree = tests.squarefree(i, primes)
             
             writer.writerow([str(i), str(countDivisors), str(countFactors),
-                             str(Fermat), str(Fibonacci), str(Gauss),
+                             str(Fermat), str(Gauss),
                              str(highlyComposite), str(mod5), str(mod6),
                              str(powerful), str(prime), str(pronic), str(squarefree)])
 
     return None
 
-main(37)
+main(2050)
