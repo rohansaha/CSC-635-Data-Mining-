2f6262acbcb1710cc3f61f10276f77f9c0af9b4d
Nathan Hall <35374924+NathanLHall@users.noreply.github.com>
2018-04-16 11:05:57 -0500

    Update Generate Data.py

--- Generate Data.py
+++ Generate Data.py
@@ -13,20 +13,18 @@ def main(index):
     with open("Data.csv", 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         
-        writer.writerow(["Number","# of Divisors","# of Factors","Euler","Even","Fermat",
-                         "Fibonacci","Gaussian Prime","Highly Composite","Lucas","Mod 5",
+        writer.writerow(["Number","# of Divisors","# of Factors","Even","Fermat",
+                         "Fibonacci","Gaussian Prime","Highly Composite","Mod 5",
                          "Mod 6","Powerful","Prime","Pronic","Squarefree"])
 
         for i in range(2, index):
             countDivisors = tests.countDivisors(i)
             countFactors = tests.countPrimeFactors(i, primes)
-            Euler = tests.Euler(i,2)
             even = tests.even(n)
             Fermat = tests.Fermat(i,2)
             Fibonacci = tests.Fibonacci(i)
             Gauss = tests.GaussianPrime(i, primes)
             highlyComposite = tests.highlyComposite(i)
-            Lucas = tests.Lucas(i)
             mod5 = tests.mod5(i)
             mod6 = tests.mod6(i)
             powerful = tests.powerful(i, primes)
@@ -34,9 +32,9 @@ def main(index):
             pronic = tests.pronic(i)
             squarefree = tests.squarefree(i, primes)
             
-            writer.writerow([str(i), str(countDivisors), str(countFactors), str(Euler),
+            writer.writerow([str(i), str(countDivisors), str(countFactors),
                              str(even), str(Fermat), str(Fibonacci), str(Gauss),
-                             str(highlyComposite), str(Lucas), str(mod5), str(mod6),
+                             str(highlyComposite), str(mod5), str(mod6),
                              str(powerful), str(prime), str(pronic), str(squarefree)])
 
     return None
