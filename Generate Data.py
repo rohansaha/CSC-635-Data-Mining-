e21afe3aa12acfe2f379027f4692ed923eb37575
Nathan Hall <35374924+NathanLHall@users.noreply.github.com>
2018-04-28 15:03:26 -0500

    Add files via upload

--- Generate Data.py
+++ Generate Data.py
@@ -1,7 +1,7 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 13 14:16:21 2018
-
 @author: NathanLHall
 """
 import csv
@@ -13,31 +13,62 @@ def main(index):
     with open("Data.csv", 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         
-        writer.writerow(["Number","# of Divisors","# of Factors","Fermat",
-                         "Gaussian Prime","Highly Composite","Mod 5",
-                         "Mod 6","Powerful","Prime","Pronic","Squarefree"])
+        writer.writerow(["Number",
+#                         "# of Divisors",
+#                         "# of Factors",
+#                         "Highly Composite",
+#                         "Powerful",
+#                         "Pronic",
+#                         "Squarefree",
+        
+#                         "Fermat",
+#                         "Fibonacci",
+#                         "Lucas",
+#                         "Mod 4",
+                         "Mod 5",
+                         "Mod 6",
+                         "Prime"
+#                         "Gaussian Prime"
+                        ])
 
         for i in range(2, index):
-            countDivisors = tests.countDivisors(i)
-            countFactors = tests.countPrimeFactors(i, primes)
-            Fermat = tests.Fermat(i,2)
-            Gauss = tests.GaussianPrime(i, primes)
-            highlyComposite = tests.highlyComposite(i)
+#            countDivisors = tests.countDivisors(i)
+#            countFactors = tests.countPrimeFactors(i, primes)
+#            highlyComposite = tests.highlyComposite(i)
+#            powerful = tests.powerful(i, primes)
+#            pronic = tests.pronic(i)
+#            squarefree = tests.squarefree(i, primes)
+
+#            Fermat = tests.Fermat(i,2)
+#            Fibonacci = tests.Fibonacci(i)
+#            Lucas = tests.Lucas(i)
+#            mod4 = tests.mod4(i)
             mod5 = tests.mod5(i)
             mod6 = tests.mod6(i)
-            powerful = tests.powerful(i, primes)
             if i in primes:
                 prime = 1
             else:
                 prime = 0
-            pronic = tests.pronic(i)
-            squarefree = tests.squarefree(i, primes)
+#            Gauss = tests.GaussianPrime(i, primes)
             
-            writer.writerow([str(i), str(countDivisors), str(countFactors),
-                             str(Fermat), str(Gauss),
-                             str(highlyComposite), str(mod5), str(mod6),
-                             str(powerful), str(prime), str(pronic), str(squarefree)])
+            writer.writerow([str(i),
+#                             str(countDivisors),
+#                             str(countFactors),
+#                             str(highlyComposite),
+#                             str(powerful),
+#                             str(pronic),
+#                             str(squarefree),
+            
+#                             str(Fermat),
+#                             str(Fibonacci),
+#                             str(Lucas),
+#                             str(mod4),
+                             str(mod5),
+                             str(mod6),
+                             str(prime)
+#                             str(Gauss)
+                             ])
 
     return None
 
-main(2050)
+main(1474)
